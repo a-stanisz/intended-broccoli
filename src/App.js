8261152c64ac084ac1dec46ce9f35c9188f070b8
a-stanisz <hello@adamstaniszewski.com>
2019-05-08 16:00:17 +0200

    Add button concept

--- src/App.js
+++ src/App.js
@@ -22,11 +22,17 @@ const list = [
 ];
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      list,
+    };
+  }
   render() {
-    const helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        {list.map(item =>
+        {this.state.list.map(item =>
             <div key={item.objectID}>
               <span>
                 <a href={item.url}>{item.title}</a>
@@ -34,6 +40,14 @@ class App extends Component {
               <span> {item.author} </span>
               <span> {item.num_comments} </span>
               <span> {item.points} </span>
+              <span>
+                <button
+                  onClick={() +> this.onDismiss(item.objectID)}
+                  type="button"
+                >
+                  Dismiss
+                </button>
+              </span>
             </div>
         )}
       </div>
